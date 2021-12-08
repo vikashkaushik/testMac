460b0034f0b2f4afe7ecedf124e503aae041c552
vikashkaushik <86675397+vikashkaushik@users.noreply.github.com>
2021-12-08 11:00:43 -0600

    finished 2.1 on 2.2

--- iOS_FundamentalsBookFiles-main/2 - Introduction to UIKit/1 - Strings/Lab - Strings.playground/Pages/4. Exercise - String Equality and Comparison.xcplaygroundpage/Contents.swift
+++ iOS_FundamentalsBookFiles-main/2 - Introduction to UIKit/1 - Strings/Lab - Strings.playground/Pages/4. Exercise - String Equality and Comparison.xcplaygroundpage/Contents.swift
@@ -16,11 +16,18 @@ let name = "vikash"
  
 - "<INSERT LOWERCASED VERSION OF `nameInCaps` HERE> and <INSERT LOWERCASED VERSION OF `name` HERE> are not the same."
  */
-
+if nameInCaps.lowercased() == name {
+    print(nameInCaps.lowercased() + " and " + name + " are the same")
+}
+else{
+    print(nameInCaps.lowercased() + " and " + name + " are not the same")
+}
 
 //:  Imagine you are looking through a list of names to find any that end in "Jr." Write an if statement below that will check if `junior` has the suffix "Jr.". If it does, print "We found a second generation name!"
 let junior = "Cal Ripken Jr."
-
+if junior.hasSuffix("Jr."){
+    print("We found a second generation name")
+}
 
 
 //:  Suppose you are trying to find a document on your computer that contains Hamlet's famous soliloquy written by Shakespeare. You write a simple app that will check every document to see if it contains the phrase "to be, or not to be". You decide to do part of this with the `contains(_:)` method. Write an if statement below that will check if `textToSearchThrough` contains `textToSearchFor`. If it does, print "I found it!" Be sure to make this functionality case insensitive.
@@ -29,8 +36,12 @@ let textToSearchThrough = "To be, or not to be--that is the question"
 let textToSearchFor = "to be, or not to be"
 
 
-//:  Print to the console the number of characters in your name by using the `count` property on `name`.
+if textToSearchThrough.lowercased().contains(textToSearchFor){
+    print("I found it")
+}
 
+//:  Print to the console the number of characters in your name by using the `count` property on `name`.
+print(name.count)
 
 /*:
 [Previous](@previous)  |  page 4 of 5  |  [Next: App Exercise - Password Entry and User Search](@next)
--- iOS_FundamentalsBookFiles-main/2 - Introduction to UIKit/2 - Functions/Lab - Functions.playground/Pages/1. Exercise - Create Functions.xcplaygroundpage/Contents.swift
+++ iOS_FundamentalsBookFiles-main/2 - Introduction to UIKit/2 - Functions/Lab - Functions.playground/Pages/1. Exercise - Create Functions.xcplaygroundpage/Contents.swift
@@ -3,10 +3,30 @@
 
  Write a function called `introduceMyself` that prints a brief introduction of yourself. Call the function and observe the printout.
  */
+func introduceMyself(intro: String){
+    print("Hello, Yo soy \(intro)!")
+}
 
+introduceMyself(intro: "Matthew")
 
 //:  Write a function called `magicEightBall` that generates a random number and then uses either a switch statement or if-else-if statements to print different responses based on the random number generated. `let randomNum = Int.random(in: 0...4)` will generate a random number from 0 to 4, after which you can print different phrases corresponding to the number generated. Call the function multiple times and observe the different printouts.
 import Foundation
+
+func magicEightBall(){
+    let randomNum = Int.random(in: 0...4)
+    if randomNum == 1{
+        print("A")
+    }
+    else if randomNum == 2{
+        print("B")
+    }
+    else if randomNum == 3{
+        print("C")
+    }
+    else if randomNum == 4{
+        print("D")
+    }
+}
 
 
 /*:
